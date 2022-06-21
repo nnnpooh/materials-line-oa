ae3a257895007ac998a22b12c560d69e5ed5073a
Nirand <nnnpooh@gmail.com>
2022-06-21 10:29:13 +0700

    update

--- temp/add-student.js
+++ temp/add-student.js
@@ -10,28 +10,37 @@ const csv = require('csvtojson');
 
 const fs = require('fs');
 
-//const csvFilePath = './student_data/studentlist_259103_001000.csv';
-//const csvFilePath = './student_data/studentlist_259103_006000.csv';
+// const csvFilePath = './student_data/studentlist_259103_001000.csv';
 const csvFilePath = './student_data/studentlist_259103_802000.csv';
 
-const data = fs.readFileSync(csvFilePath, 'utf8');
-console.log(data);
+// const data = fs.readFileSync(csvFilePath, 'utf8');
+// console.log(data);
 
 async function addData() {
   const students = await csv().fromFile(csvFilePath);
-  //console.log(students);
+  // console.log(students);
 
-  try {
+  students.forEach(async (student) => {
+    console.log(
+      `Processing ${student.cmu_id}: ${student.firstname} ${student.lastname}`
+    );
     const { data, error } = await supabase
       .from('registered_students')
-      .insert(students);
+      .insert(student);
 
     if (error) {
-      console.log(error);
+      console.log(error.details);
+      const { data2, error2 } = await supabase
+        .from('registered_students')
+        .update(student)
+        .match({ cmu_id: student.cmu_id });
+      if (error2) {
+        console.log('Cannot update.', error.details);
+      } else {
+        console.log('Update data successfully.');
+      }
     }
-  } catch (error) {
-    console.log(error);
-  }
+  });
 }
 
 addData();
