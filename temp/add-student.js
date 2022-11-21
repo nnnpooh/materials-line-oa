2213b0aca3cabcdbaba64f17bbd382d5d78db2d2
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-11-22 04:40:15 +0700

    Update

--- temp/add-student.js
+++ temp/add-student.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
-const dateFNS = require('date-fns');
-const { createClient } = require('@supabase/supabase-js');
+const dateFNS = require("date-fns");
+const { createClient } = require("@supabase/supabase-js");
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const serviceKey = process.env.SERVICE_KEY;
 const supabase = createClient(supabaseUrl, serviceKey);
-const csv = require('csvtojson');
+const csv = require("csvtojson");
 
-const fs = require('fs');
+const fs = require("fs");
 
-// const csvFilePath = './student_data/studentlist_259103_001000.csv';
-const csvFilePath = './student_data/studentlist_259103_802000.csv';
+const csvFilePath = "./student_data/studentlist_259103_003000.csv";
 
-// const data = fs.readFileSync(csvFilePath, 'utf8');
-// console.log(data);
+const data = fs.readFileSync(csvFilePath, "utf8");
+console.log(data);
 
 async function addData() {
   const students = await csv().fromFile(csvFilePath);
@@ -25,19 +24,19 @@ async function addData() {
       `Processing ${student.cmu_id}: ${student.firstname} ${student.lastname}`
     );
     const { data, error } = await supabase
-      .from('registered_students')
+      .from("registered_students")
       .insert(student);
 
     if (error) {
       console.log(error.details);
       const { data2, error2 } = await supabase
-        .from('registered_students')
+        .from("registered_students")
         .update(student)
         .match({ cmu_id: student.cmu_id });
       if (error2) {
-        console.log('Cannot update.', error.details);
+        console.log("Cannot update.", error.details);
       } else {
-        console.log('Update data successfully.');
+        console.log("Update data successfully.");
       }
     }
   });
