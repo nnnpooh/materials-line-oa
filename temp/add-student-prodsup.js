e8642a12712b0e148c4cfa168d795acebca8c49a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2022-12-08 05:09:36 +0700

    Update

--- temp/add-student-prodsup.js
+++ temp/add-student-prodsup.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
-const dateFNS = require('date-fns');
-const { createClient } = require('@supabase/supabase-js');
+const dateFNS = require("date-fns");
+const { createClient } = require("@supabase/supabase-js");
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const serviceKey = process.env.SERVICE_KEY;
 const supabase = createClient(supabaseUrl, serviceKey);
-const csv = require('csvtojson');
+const csv = require("csvtojson");
 
-const fs = require('fs');
+const fs = require("fs");
 
-const csvFilePath = './temp/student_data/255217.csv';
+const csvFilePath = "./student_data/studentlist_255217_formatted.csv";
 
-// const data = fs.readFileSync(csvFilePath, 'utf8');
+// const data = fs.readFileSync(csvFilePath, "utf8");
 // console.log(data);
 
 async function addRegisteredData() {
@@ -40,7 +40,7 @@ async function addRegisteredData() {
 
   try {
     const { data, error } = await supabase
-      .from('registered_students')
+      .from("registered_students")
       .insert(registeredData);
 
     if (error) {
@@ -51,7 +51,7 @@ async function addRegisteredData() {
   }
 
   try {
-    const { data, error } = await supabase.from('users').insert(userData);
+    const { data, error } = await supabase.from("users").insert(userData);
 
     if (error) {
       console.log(error);
